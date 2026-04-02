4deb68db7aae94a05351be55f3960fa8e916befb
Pierito-cpu <24100634@ue.edu.pe>
2026-04-02 13:39:54 -0500

    Create Fecha.dart

---
+++ Fecha.dart
@@ -0,0 +1,159 @@
+import '/flutter_flow/flutter_flow_calendar.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import 'elegir_fecha_model.dart';
+export 'elegir_fecha_model.dart';
+
+class ElegirFechaWidget extends StatefulWidget {
+  const ElegirFechaWidget({super.key});
+
+  @override
+  State<ElegirFechaWidget> createState() => _ElegirFechaWidgetState();
+}
+
+class _ElegirFechaWidgetState extends State<ElegirFechaWidget> {
+  late ElegirFechaModel _model;
+
+  @override
+  void setState(VoidCallback callback) {
+    super.setState(callback);
+    _model.onUpdate();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => ElegirFechaModel());
+  }
+
+  @override
+  void dispose() {
+    _model.maybeDispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 420,
+      decoration: BoxDecoration(
+        color: FlutterFlowTheme.of(context).secondaryBackground,
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 5,
+            color: Color(0x3B1D2429),
+            offset: Offset(
+              0.0,
+              -3,
+            ),
+          )
+        ],
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(16),
+          topRight: Radius.circular(16),
+        ),
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            FlutterFlowCalendar(
+              color: FlutterFlowTheme.of(context).primary,
+              iconColor: FlutterFlowTheme.of(context).secondaryText,
+              weekFormat: false,
+              weekStartsMonday: false,
+              rowHeight: 48,
+              onChange: (DateTimeRange? newSelectedDate) async {
+                if (_model.calendarSelectedDay == newSelectedDate) {
+                  return;
+                }
+                _model.calendarSelectedDay = newSelectedDate;
+                FFAppState().fechaReporte = _model.calendarSelectedDay?.start;
+                safeSetState(() {});
+                Navigator.pop(context);
+                safeSetState(() {});
+              },
+              titleStyle: FlutterFlowTheme.of(context).titleLarge.override(
+                    font: GoogleFonts.interTight(
+                      fontWeight:
+                          FlutterFlowTheme.of(context).titleLarge.fontWeight,
+                      fontStyle:
+                          FlutterFlowTheme.of(context).titleLarge.fontStyle,
+                    ),
+                    letterSpacing: 0.0,
+                    fontWeight:
+                        FlutterFlowTheme.of(context).titleLarge.fontWeight,
+                    fontStyle:
+                        FlutterFlowTheme.of(context).titleLarge.fontStyle,
+                  ),
+              dayOfWeekStyle: FlutterFlowTheme.of(context).bodyLarge.override(
+                    font: GoogleFonts.inter(
+                      fontWeight:
+                          FlutterFlowTheme.of(context).bodyLarge.fontWeight,
+                      fontStyle:
+                          FlutterFlowTheme.of(context).bodyLarge.fontStyle,
+                    ),
+                    letterSpacing: 0.0,
+                    fontWeight:
+                        FlutterFlowTheme.of(context).bodyLarge.fontWeight,
+                    fontStyle: FlutterFlowTheme.of(context).bodyLarge.fontStyle,
+                  ),
+              dateStyle: FlutterFlowTheme.of(context).bodyMedium.override(
+                    font: GoogleFonts.inter(
+                      fontWeight:
+                          FlutterFlowTheme.of(context).bodyMedium.fontWeight,
+                      fontStyle:
+                          FlutterFlowTheme.of(context).bodyMedium.fontStyle,
+                    ),
+                    letterSpacing: 0.0,
+                    fontWeight:
+                        FlutterFlowTheme.of(context).bodyMedium.fontWeight,
+                    fontStyle:
+                        FlutterFlowTheme.of(context).bodyMedium.fontStyle,
+                  ),
+              selectedDateStyle: FlutterFlowTheme.of(context)
+                  .titleSmall
+                  .override(
+                    font: GoogleFonts.interTight(
+                      fontWeight:
+                          FlutterFlowTheme.of(context).titleSmall.fontWeight,
+                      fontStyle:
+                          FlutterFlowTheme.of(context).titleSmall.fontStyle,
+                    ),
+                    letterSpacing: 0.0,
+                    fontWeight:
+                        FlutterFlowTheme.of(context).titleSmall.fontWeight,
+                    fontStyle:
+                        FlutterFlowTheme.of(context).titleSmall.fontStyle,
+                  ),
+              inactiveDateStyle: FlutterFlowTheme.of(context)
+                  .labelMedium
+                  .override(
+                    font: GoogleFonts.inter(
+                      fontWeight:
+                          FlutterFlowTheme.of(context).labelMedium.fontWeight,
+                      fontStyle:
+                          FlutterFlowTheme.of(context).labelMedium.fontStyle,
+                    ),
+                    letterSpacing: 0.0,
+                    fontWeight:
+                        FlutterFlowTheme.of(context).labelMedium.fontWeight,
+                    fontStyle:
+                        FlutterFlowTheme.of(context).labelMedium.fontStyle,
+                  ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
